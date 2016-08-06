b87e6954d96bb1e916bcaaaaed9e8e84f3f0d785
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-08-05 20:53:51 -0700

    add callback function for getting repository objects

--- js/github-user-lookup.js
+++ js/github-user-lookup.js
@@ -3,7 +3,8 @@ var apiKey = require('./../.env').apiKey;
 function User() {
   this.avatar_url = null;
   this.name = null;
-  this.public_repos = null;
+  this.repos_name = [];
+  this.repos_description = [];
 }
 
 User.prototype.getRepos = function() {
@@ -12,15 +13,17 @@ User.prototype.getRepos = function() {
     console.log(user);
     self.avatar_url = user.avatar_url;
     self.name = user.name;
-  }).then(function(){
-      $('#username').append('<p>' + self.name + '</p><img src="' + self.avatar_url + '" alt="user avatar">');
+    $('#user').append('<p>' + user.name + '</p><img src="' + user.avatar_url + '" alt="user avatar">');
   });
   $.get('https://api.github.com/users/daneden/repos?access_token=' + apiKey).then(function(repos) {
     console.log(repos);
+    self.repos_name = repos.name;
+    self.repos_description = repos.description;
+    repos.forEach(function(repo) {
 
-  }).then(function(){
-
-      // $('#repository').append('<p>' + self.repos_url + '</p>');
+      $('#repository').append('<div class="row"><div class="twelve columns"><p>' + repo.name + '</p></div><div class="eleven columns description"><p>' + repo.description + '</p></div>');
+      $('#description').append('');
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
