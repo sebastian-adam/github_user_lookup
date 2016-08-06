099442d3ad9ee7cde02ea08eb4cf4c75a75b3bd0
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-08-05 18:36:25 -0700

    add succesful callback function for a users repos

--- js/github-user-lookup.js
+++ js/github-user-lookup.js
@@ -8,14 +8,19 @@ function User() {
 
 User.prototype.getRepos = function() {
   var self = this;
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-    self.avatar_url = response.avatar_url;
-    self.name = response.name;
-    self.public_repos = response.public_repos;
-  }).then(function(response){
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(user){
+    console.log(user);
+    self.avatar_url = user.avatar_url;
+    self.name = user.name;
+  }).then(function(){
       $('#username').append('<p>' + self.name + '</p><img src="' + self.avatar_url + '" alt="user avatar">');
+  });
+  $.get('https://api.github.com/users/daneden/repos?access_token=' + apiKey).then(function(repos) {
+    console.log(repos);
 
+  }).then(function(){
+
+      // $('#repository').append('<p>' + self.repos_url + '</p>');
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
